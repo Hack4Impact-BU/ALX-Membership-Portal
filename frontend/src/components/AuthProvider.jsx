84ba7b0bfdc66847e96d545e8f782c3190d8e719
andeliuliu <andelerliutv@gmail.com>
2024-10-03 10:22:00 -0400

    added most recent changes

--- frontend/src/components/AuthProvider.jsx
+++ frontend/src/components/AuthProvider.jsx
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const AuthProvider = ({ children }) => {
-  const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN; // Replace with the domain i put in Slack own domain
-  const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID; // Replace with the client id i put in Slack own client id
+  const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN; // Replace with the domain i put in Slack own domain (put into .env file)
+  const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID; // Replace with the client id i put in Slack own client id (put into .env file)
 
   const onRedirectCallback = (appState) => {
     window.history.replaceState({}, document.title, appState?.returnTo || '/');
