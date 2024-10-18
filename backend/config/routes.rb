52d82b523a543b27b188f0643dd11fb9a56e1cd0
stargazerJerry2023 <147276585+stargazerJerry2023@users.noreply.github.com>
2024-10-18 18:44:42 -0400

    Implemented User Login and Sign Up, Token System for Authorization

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,13 +4,21 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get '/profile', to: 'users#profile'
 
+  post 'auth0/sign_up', to: 'auth0#sign_up'
+  post 'auth0/login', to: 'auth0#login'
+  
   # Defines the root path route ("/")
   # root "posts#index"
-  Rails.application.routes.draw do
+
     # Other routes
   
+   
     # API endpoint to get test_table data
     get '/users', to: 'users#index'
-  end
+
+
+
+
 end
