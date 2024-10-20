358896871a401dcbe5cf78f09c176169af9a8140
andeliuliu <andelerliutv@gmail.com>
2024-10-19 21:37:59 -0400

    added 0.0.0.0

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -22,8 +22,7 @@ module Backend
     # CORS configuration
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'  # Your frontend's URL
-
+        origins 'http://localhost:3000'  , 'http://0.0.0.0:3000'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
