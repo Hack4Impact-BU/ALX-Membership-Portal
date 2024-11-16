f2c3e234d470c55468cb7113eb54cde6e62f671f
nolan <144292887+nclan1@users.noreply.github.com>
2024-11-16 18:51:54 -0500

    removed border

--- frontend/src/app/benefits_discounts/page.jsx
+++ frontend/src/app/benefits_discounts/page.jsx
@@ -5,7 +5,7 @@ import CardList from "./components/cardList/CardList"
 export default function BenefitsDiscounts() {
 
     return (
-        <section className="w-full flex h-full flex-col border ">
+        <section className="w-full flex h-full flex-col ">
         <div className="w-3/4 mt-32 ">
             <ReusableHeader header={"Product Offers"} translation={"* Ofertas de Productos"}></ReusableHeader>
         </div>
