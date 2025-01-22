726d090c72ef59f8dd31e017c1799f0d1639a8a6
andeliuliu <andelerliutv@gmail.com>
2025-01-22 18:38:15 -0500

    fixed questions page

--- frontend/src/app/questions/page.jsx
+++ frontend/src/app/questions/page.jsx
@@ -1,16 +1,16 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import Hyperlinks from '@/components/Hyperlinks';
 import { Inter, Proza_Libre } from 'next/font/google';
 import axios from 'axios';
 
-const inter = Inter({ subsets: ["latin"] });
-const prozaLibre = Proza_Libre({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800"] });
+const inter = Inter({ subsets: ['latin'] });
+const prozaLibre = Proza_Libre({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800'] });
 
 import { QuestionMarkCircleIcon } from '@heroicons/react/outline';
 
 export default function Questions() {
-  const router = useRouter();
   const [qAndAs, setQAndAs] = useState([]);
   const [showDelete, setShowDelete] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
@@ -40,7 +40,7 @@ export default function Questions() {
     try {
       // Send a DELETE request to your API
       await axios.delete(`${process.env.NEXT_PUBLIC_API_BASE_URL}/q_and_as/${id}`);
-      
+
       // Remove the deleted item from the local state
       setQAndAs(qAndAs.filter((qanda) => qanda.id !== id));
     } catch (error) {
@@ -56,19 +56,19 @@ export default function Questions() {
         <QuestionMarkCircleIcon className="h-32 w-32 text-white mx-2" />
         <h1 className="text-7xl font-bold font-custom pt-10">FAQ's</h1>
       </div>
-      
+
       <div className="w-11/12 rounded-lg p-8">
         {/* Display Error Message */}
         {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
 
         {/* Buttons for Create and Delete */}
         <div className="flex gap-4 mb-6">
-          <button
-            onClick={() => router.push('questions/create')} // Replace with actual route to the Create Q&A component
-            className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors"
-          >
-            Create Q&A
-          </button>
+          {/* Replace router.push with Link for routing */}
+          <Link href="questions/create" passHref>
+            <p className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors cursor-pointer">
+              Create Q&A
+            </p>
+          </Link>
           <button
             onClick={toggleDeleteMode}
             className={`bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-700 transition-colors ${showDelete ? 'opacity-70' : ''}`}
