5cb670836a98e34aa21e31f60fee3771cf6e91a6
nolan <144292887+nclan1@users.noreply.github.com>
2025-02-11 17:32:58 -0500

    added routes in backend

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :eventlists
   resources :jobs, only: [:index, :create, :show]
   resources :q_and_as, only: [:index, :show, :new, :create, :destroy]
+  resources :research
 
   # Custom routes
   get "up" => "rails/health#show", as: :rails_health_check
