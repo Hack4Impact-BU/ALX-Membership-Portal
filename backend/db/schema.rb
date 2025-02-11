1af7e32cd25aebd3654eafc13c847776919e2da3
andeliuliu <andelerliutv@gmail.com>
2025-02-11 13:46:05 -0500

    fixed credentials and stuff

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.2].define(version: 2025_01_02_054422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,30 +40,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_02_054422) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "benefits_discounts", force: :cascade do |t|
-    t.string "title"
-    t.string "location"
-    t.date "start_date"
-    t.date "end_date"
-    t.text "offer_description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "link"
-    t.boolean "is_saved", default: false, null: false
-    t.string "business_type"
-  end
-
-  create_table "discounts", force: :cascade do |t|
-    t.string "title"
-    t.string "location"
-    t.boolean "is_saved"
-    t.string "offer_description"
-    t.string "redeem_desc"
-    t.date "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "eventlists", force: :cascade do |t|
@@ -108,13 +83,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_02_054422) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "members", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 100, null: false
-    t.string "email", limit: 100, null: false
-    t.date "join_date", default: -> { "CURRENT_DATE" }
-
-    t.unique_constraint ["email"], name: "members_email_key"
 
   create_table "q_and_as", force: :cascade do |t|
     t.string "question"
