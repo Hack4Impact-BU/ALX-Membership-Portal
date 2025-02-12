e58511c4c61cb6b0e14e607d6b6938e11077c52e
andeliuliu <andelerliutv@gmail.com>
2025-02-12 11:23:42 -0500

    updated render build to have more logs

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
+# Enable tracing
+set -x
 
 bundle install
 # bundle exec rails assets:precompile
@@ -9,7 +11,6 @@ bundle install
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
-
 bundle exec rails db:migrate
 
 bundle exec rails db:seed
