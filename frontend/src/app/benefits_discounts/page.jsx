249501a7c9c49384843cf6d1811bfc18bf7662aa
Gerardo Vega <147276585+stargazerJerry2023@users.noreply.github.com>
2025-03-30 00:28:28 -0400

    Update page.jsx

--- frontend/src/app/benefits_discounts/page.jsx
+++ frontend/src/app/benefits_discounts/page.jsx
@@ -1,16 +1,17 @@
 
 import ReusableHeader from "@/components/ReusableHeader/ReusableHeader"
 import CardList from "./components/cardList/CardList"
+import Hyperlinks from "@/components/Hyperlinks"
 
 export default function BenefitsDiscounts() {
 
     return (
         <section className="w-full flex h-full flex-col items-center">
             <div className="w-11/12 mt-20">
-                <ReusableHeader header={"Product Offers"} translation={"* Ofertas de Productos"}></ReusableHeader>
+                <ReusableHeader header={"Product Offers"} translation={"* Ofertas de Productos"} isAdmin={true} directTo={"/benefits_discounts/create"}/>
             </div>
             <CardList />
-
+            <Hyperlinks />
 
         </section>
     )
