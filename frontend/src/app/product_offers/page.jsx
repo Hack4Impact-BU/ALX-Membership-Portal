e73d25879c75c706ee089b33655cb09b62f79951
nolan <144292887+nclan1@users.noreply.github.com>
2025-04-16 13:55:07 -0400

    minor

--- frontend/src/app/product_offers/page.jsx
+++ frontend/src/app/product_offers/page.jsx
@@ -15,7 +15,7 @@ export default function BenefitsDiscounts() {
 
     return (
         <section className="w-full flex h-full flex-col items-center">
-            <div className="w-11/12 mt-40 mb-[-5rem]">
+            <div className="w-11/12 mt-20 mb-[-5rem]">
                 <ReusableHeader header={"Product Offers"} translation={"* Ofertas de Productos"} isAdmin={isAdmin} directTo={"/benefits_discounts/create"}/>
             </div>
             <CardList isAdmin={isAdmin} />
